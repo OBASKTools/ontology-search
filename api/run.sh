805d5ab0f2fc368cce45387b8bb456b87f827e9d
Ismail Ugur Bayindir <ubyndr@gmail.com>
2023-04-05 11:06:38 +0100

    Update run.sh

--- api/run.sh
+++ api/run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Deprecation warning! Install Bazel and use "./sandbox/bazel.sh run //api:service" command instaed!
-
 set -e
-docker build -t cap/search-service -f docker/Dockerfile .
-docker run -d -p 8081:8081 --network cap-pipeline-config_default --name cap-search -it cap/search-service
+docker build -t obasktools/ontologysearch -f docker/Dockerfile .
+docker run -d -p 8081:8081 --network obask_default --name cap-search -it obasktools/ontologysearch
